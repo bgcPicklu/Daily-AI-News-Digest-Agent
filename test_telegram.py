f60479e41e3ff334a94817dc1e27bf9b101b56a9
bgcPicklu <shoumendatasoftbd@gmail.com>
2026-06-29 17:49:14 +0600

    Add test_telegram to test telegram,slack & mail

---
+++ test_telegram.py
@@ -0,0 +1,26 @@
+# import asyncio
+# from telegram import Bot
+
+# TOKEN = "xxxxxxxxxxx:xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+# CHAT_ID = "xxxxxxxx"
+
+# async def main():
+
+#     bot = Bot(token=TOKEN)
+
+#     await bot.send_message(
+#         chat_id=CHAT_ID,
+#         text="Telegram Test Successful"
+#     )
+
+# asyncio.run(main())
+
+# from app.notifications.email_sender import send_email
+
+# send_email("Email Test Successful")
+
+# from app.notifications.slack_sender import send_slack
+
+# send_slack(
+#     "✅ AI News Digest Slack Test Successful"
+# )
